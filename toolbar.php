48049b2cf13af6334041fcc28f85a528cb11ab27
cqrt <everythingairsoft@gmail.com>
2013-05-26 05:19:03 +1200

    Create toolbar.php

---
+++ toolbar.php
@@ -0,0 +1,11 @@
+<?php global $link; ?>
+<?php
+  echo stylesheet_tag("toolbar.css");
+	print_user_stylesheet($link)
+?>
+		<button class="button_nav" title="Subscribe" onclick="quickMenuGo('qmcAddFeed')"></button>
+		<button class="button_nav" title="Toggle" onclick="toggle_cdm_expanded()"></button>
+		<button class="button_nav" title="Mark" onclick="catchupCurrentFeed()"></button>
+		<button class="button_nav" title="Refresh" onclick="viewCurrentFeed()"></button>
+		<button class="button_nav" title="Previous" onclick="moveToPost('prev',true)"></button>
+		<button class="button_nav" title="Next" onclick="moveToPost('next',true)"></button>
