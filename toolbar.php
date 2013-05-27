0f667510f89e55cc9d0eaedeb04a4074835b9b67
cqrt <everythingairsoft@gmail.com>
2013-05-27 16:15:22 +1200

    Update toolbar.php

--- toolbar.php
+++ toolbar.php
@@ -3,11 +3,9 @@
   echo stylesheet_tag("/plugins/feedspin_toolbar/toolbar.css");
 	print_user_stylesheet($link)
 ?>
-<ul>
 <li class="rss"><a title="Subscribe" onclick="quickMenuGo('qmcAddFeed')"></a></li>
 <li class="list"><a title="Toggle" onclick="toggle_cdm_expanded()"></a></li>
 <li class="update"><a title="Mark" onclick="catchupCurrentFeed()"></a></li>
 <li class="repeat"><a title="Refresh" onclick="viewCurrentFeed()"></a></li>
 <li class="arrow-alt up"><a title="Previous" onclick="moveToPost('prev',true)"></a></li>
 <li class="arrow-alt down"><a title="Next" onclick="moveToPost('next',true)"></a></li>
-</ul>
