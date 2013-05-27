932521c5f5877d2bd63a142655ae2d611877c0e4
cqrt <everythingairsoft@gmail.com>
2013-05-27 13:54:59 +1200

    Update toolbar.php

--- toolbar.php
+++ toolbar.php
@@ -7,7 +7,7 @@
 <li class="rss"><a title="Subscribe" onclick="quickMenuGo('qmcAddFeed')"></a></li>
 <li class="list"><a title="Toggle" onclick="toggle_cdm_expanded()"></a></li>
 <li class="success"><a title="Mark" onclick="catchupCurrentFeed()"></a></li>
-<li class="repeat"><a title="Refresh" onclick="viewCurrentFeed()">R</a></li>
+<li class="repeat"><a title="Refresh" onclick="viewCurrentFeed()"></a></li>
 <li class="previous"><a title="Previous" onclick="moveToPost('prev',true)">P</a></li>
 <li class="next"><a title="Next" onclick="moveToPost('next',true)">N</a></li>
 </ul>
