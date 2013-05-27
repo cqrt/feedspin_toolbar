56a26df550a165cc91e1192ec32ddccafd66886f
cqrt <everythingairsoft@gmail.com>
2013-05-27 13:53:06 +1200

    Update toolbar.php

--- toolbar.php
+++ toolbar.php
@@ -4,10 +4,10 @@
 	print_user_stylesheet($link)
 ?>
 <ul>
-<li class="rss"><a title="Subscribe" onclick="quickMenuGo('qmcAddFeed')">S</a></li>
-<li class="list"><a title="Toggle" onclick="toggle_cdm_expanded()">T</a></li>
-<li class="success"><a title="Mark" onclick="catchupCurrentFeed()">M</a></li>
-<li class="refresh"><a title="Refresh" onclick="viewCurrentFeed()">R</a></li>
+<li class="rss"><a title="Subscribe" onclick="quickMenuGo('qmcAddFeed')"></a></li>
+<li class="list"><a title="Toggle" onclick="toggle_cdm_expanded()"></a></li>
+<li class="success"><a title="Mark" onclick="catchupCurrentFeed()"></a></li>
+<li class="repeat"><a title="Refresh" onclick="viewCurrentFeed()">R</a></li>
 <li class="previous"><a title="Previous" onclick="moveToPost('prev',true)">P</a></li>
 <li class="next"><a title="Next" onclick="moveToPost('next',true)">N</a></li>
 </ul>
