6bd35367e56a2c8441bc11e10db24a81fa8eedf8
cqrt <everythingairsoft@gmail.com>
2013-05-28 00:13:52 +1200

    Update toolbar.php

--- toolbar.php
+++ toolbar.php
@@ -4,8 +4,8 @@
 	print_user_stylesheet($link)
 ?>
 <div class="rss_container"><li class="rss"><a title="Subscribe" onclick="quickMenuGo('qmcAddFeed')"></a></li></div>
-<li class="list"><a title="Toggle" onclick="toggle_cdm_expanded()"></a></li>
-<li class="update"><a title="Mark" onclick="catchupCurrentFeed()"></a></li>
-<li class="repeat"><a title="Refresh" onclick="viewCurrentFeed()"></a></li>
-<li class="arrow-alt up"><a title="Previous" onclick="moveToPost('prev',true)"></a></li>
-<li class="arrow-alt down"><a title="Next" onclick="moveToPost('next',true)"></a></li>
+<div class="list_container"><li class="list"><a title="Toggle" onclick="toggle_cdm_expanded()"></a></li></div>
+<div class="update_container"><li class="update"><a title="Mark" onclick="catchupCurrentFeed()"></a></li></div>
+<div class="repeat_container"><li class="repeat"><a title="Refresh" onclick="viewCurrentFeed()"></a></li></div>
+<div class="up_container"><li class="arrow-alt up"><a title="Previous" onclick="moveToPost('prev',true)"></a></li></div>
+<div class="down_container"><li class="arrow-alt down"><a title="Next" onclick="moveToPost('next',true)"></a></li></div>
